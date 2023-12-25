49c2ab50b99e27fdb079055e37686a7565f19bdb
Jordan Atkins <duderad37@gmail.com>
2023-12-25 22:00:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-let age = prompt("How old are you?");
-typeConversion(age);
+// let age = prompt("How old are you?");
+// typeConversion(age);
+
+// let num = prompt("Enter a number");
+// mathModulus(num);
+
+// comparisons();
+
+//conditionals();
+
+multipleConditions();
+
+function multipleConditions() {
+    
+    alert("We're gonna test your age");
+    let age = prompt('age?', 18);
+
+    let message = (age < 3) ? 'Hi, baby!' :
+    (age < 18) ? 'Hello!' :
+    (age < 100) ? 'Greetings!' :
+    'What an unusual age!';
+
+    alert( message );
+}
+
+function conditionals(_num1, _num2) {
+    
+    let year = prompt('In which year was ECMAScript-2015 specification published?', '');
+    if (year == 2015) alert( 'You are right!' )
+    else { alert(`You're wrong!`) };
+
+    _num1 = prompt("Enter a number for a test");
+    _num2 = prompt(`Enter another number for the test, your 1st number was ${_num1}`);
+
+    alert(`We are going to test whether the 1st number is bigger than the 2nd, Okay?`);
+    alert(`This statement is: ${_num1 > _num2}`);
+}
+
+function comparisons() {
+    
+    alert( 2 > 1 ); //true
+    alert( 2 == 1 ); //false
+    alert( 2 != 1 ); //true
+
+    alert( 'Z' > 'A' ); //true
+    alert( 'Glow' > 'Glee' ); //true
+    alert( 'Bee' > 'Be' ); //true
+}
+
+function mathModulus(num) {
+    
+    alert(`Remainder of ${num}/${3} is: ${Number(num % 4)}`);
+
+}
 
 function typeConversion(age) {
     
